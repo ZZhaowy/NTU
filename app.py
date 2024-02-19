d115e3759bd30b2ec73bd323c55f710781f4e482
Zhaowy <125205266+ZZhaowy@users.noreply.github.com>
2024-02-19 12:21:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask,request, render_template
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
-           def index():
-             return(render_template("index.html"))
+def index():
+           return(render_template("index.html"))
 
 
 
