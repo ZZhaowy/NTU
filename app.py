bfb9c8ecf53f4522202b1ec9da045d9dddc3bd2e
Zhaowy <125205266+ZZhaowy@users.noreply.github.com>
2024-02-20 14:50:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
            return(render_template("index.html"))
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/main",methods=["GET","POST"])
 def main():
            r = request.form.get("r")
            return(render_template("main.html",r = r))
