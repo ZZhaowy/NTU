612c25dddbeb11b372307cb30320077fce57ab6d
Zhaowy <125205266+ZZhaowy@users.noreply.github.com>
2024-02-20 14:45:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ def index():
 
 @app.route("/",methods=["GET","POST"])
 def main():
-           name = request.form.get("name")
-           return(render_template("main.html",r = name))
+           r = request.form.get("r")
+           return(render_template("main.html",r = r))
 
 
 
