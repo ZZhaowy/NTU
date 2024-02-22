57f937da093f5a6ab7a061dc7e19253b919323fa
ZZhaowy <2909198748@qq.com>
2024-02-22 10:07:07 +0800

    2.22

--- app.py
+++ app.py
@@ -3,18 +3,25 @@ from flask import Flask, request, render_template
 import replicate
 import os
 import time
+from openai import OpenAI
+
+
+
+model = OpenAI(api_key="sk-qx4zKKzHnYRc7k8LuXyKHXdQi0gf5BcisgXBVZcXc9t4K8E9",
+               base_url="https://api.chatanywhere.tech/v1")
+os.environ["REPLICATE_API_TOKEN"] = "r8_GHyHGLZOntMYvtEgUZiE9vFdtMZrj7L2ZTtgM"
 
 app = Flask(__name__)
-os.environ["REPLICATE_API_TOKEN"] = "r8_GHyHGLZOntMYvtEgUZiE9vFdtMZrj7L2ZTtgM"
 
 r = ""
 first_time = 1
 
+# index
 @app.route("/", methods=["GET", "POST"])
 def index():
     return (render_template("index.html"))
 
-
+#main
 @app.route("/main", methods=["GET", "POST"])
 def main():
     global r, first_time
@@ -23,7 +30,7 @@ def main():
         first_time = 0
     return (render_template("main.html", r=r))
 
-
+#image
 @app.route("/image_gpt", methods=["GET", "POST"])
 def image_gpt():
     return (render_template("image_gpt.html"))
@@ -35,10 +42,29 @@ def image_result():
         input={ 
         "prompt": q,}
 )
-    time.sleep(10)
+    time.sleep(5)
     return (render_template("image_result.html",r=r[0]))
 
+#text
+@app.route("/text_gpt", methods=["GET", "POST"])
+def text_gpt():
+    return (render_template("text_gpt.html"))
 
+@app.route("/text_result", methods=["GET", "POST"])
+def text_result():
+    q = request.form.get("q")
+    r = model.chat.completions.create(
+    model="gpt-3.5-turbo", 
+    messages=[
+        {"role": "user",
+         "content": q
+        }
+    ],
+)
+    time.sleep(5)
+    return (render_template("text_result.html",r=r.choices[0].message.content))
+
+# end
 @app.route("/end", methods=["GET", "POST"])
 def end():
     global first_time
